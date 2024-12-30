b24b589f91ab9cfb0dd490b63fcd51c7fca97206
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-30 13:46:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys;c=""
-p=m=l=instr=sinstr=sp=0;ts=[];s1=[];s2=[];os=[];ins=[];i="";s=[s1,s2]
+p=m=instr=sinstr=sp=ins=0;ts=[];s1=[];s2=[];os=[];i="";s=[s1,s2]
 arg=sys.argv[0::];d=0
 while arg[0][0]!="-":
  arg.pop(0)
@@ -12,10 +12,8 @@ while arg!=[]:
   elif arg[1]=="text":c=arg[2];arg.pop(0);arg.pop(0)
  else:print(f"Invalid argument \"{arg[0]}\"");break
  arg.pop(0)
-if i!="":
- for inp in i:ins.append(inp)
 while p<len(c):
- if c[p]=="#":instr+=1;break
+ if c[p]=="#":break
  elif c[p]=="'":m=1;instr+=1
  elif m==1:
   while c[p]!="'":ts.append(c[p]);p+=1
@@ -26,7 +24,7 @@ while p<len(c):
  elif c[p]=="@":s[sp].reverse();print(s[sp][0],end="");s[sp].reverse();instr+=1
  elif c[p]=="!":s.pop();instr+=1
  elif c[p]=="_":
-  if len(ins)>0:s[sp].append(ins[0]);ins.pop(0);instr+=1
+  if len(i)>0:s[sp].append(i[ins]);instr+=1;ins+=1
   else:print("\nEOI reached",end="");break
  elif c[p]=="|":
   instr+=1
@@ -41,15 +39,14 @@ while p<len(c):
  elif c[p]=="*":
   if sp==0:sp=1
   elif sp==1:sp=0
- elif c[p]=="?":
-  if c[p+1]=="b":
-   if s1==s2:p+=3
-   else:
-    while c[p]!="]":p+=1
+ elif c[p]=="\n":sinstr=instr
  p+=1
  if p==len(c):
-  p=0;sinstr=instr
-  if l==0:instr=0
+  while 1:
+   p-=1
+   if c[p]!="\n":break
+   elif p>0:break
+  instr=sinstr
 if instr!=0:sinstr=instr
-if d==1:print("\n"+c,s1,s2,i,sinstr,"instructions",arg,end="")
+if d==1:print(f"\n\"{c}\"",s1,s2,i,sinstr,"instructions",arg)
 else:pass
